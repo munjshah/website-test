010c16a0662cb96c5584ecab41dc5b6f41450b4e
munjshah <Munjal.Shah@nrel.gov>
2025-08-11 15:03:48 -0600

    changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openTab(evt, tabName) {
   document.querySelectorAll('.tabcontent').forEach(tab => tab.style.display = 'none');
-  document.querySelectorAll('.tablink').forEach(btn => btn.style.background = '#444');
+  document.querySelectorAll('.tablink').forEach(btn => btn.classList.remove('active'));
   document.getElementById(tabName).style.display = 'block';
-  evt.currentTarget.style.background = '#666';
+  evt.currentTarget.classList.add('active');
 }
 document.getElementById("defaultTab").click();
